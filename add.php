712097142cbf66bf7bf5ac26a5ddd77921a2972d
Gitanito <nico.spataro@gmail.com>
2024-01-08 12:06:54 +0100

    a

--- add.php
+++ add.php
@@ -5,18 +5,29 @@
 
         $intext = explode(PHP_EOL, $_POST['mytext']);
 
+
+        $sources = [];
+
         foreach($intext as $line) {
-            if (trim($line) != "") {
-                if (substr($line,0,1) === "[") { // this a citation-line
-                    $line = str_replace("[", "[^", $line);
-                    $line = str_replace("]", "]:", $line);
-                } else {
-                    $line = str_replace("[", "[^", $line);
-                }
+            if (substr($line,0,1) === "[") { // this a citation-line
+                $l = explode(" ", $line);
+                $index = str_replace(['[',']'], "", $l[0]);
+                $sources[$index] = '<a name="cit'.$index.'">['.$index.']</a> <a target=_blank href="'.trim($l[1]).'">'.trim($l[1]).'</a>';
+            } else {
+                $outtext[] = $line;
             }
-            $outtext[] = $line;
         }
-        file_put_contents('sources/test.md', join(PHP_EOL, $outtext));
+print_r($sources);
+        foreach($outtext as $tkey => $tline) {
+            foreach($sources as $k => $s) {
+                $tline = str_replace('['.$k.']', '<sup><a href="#cit'.$k.'">['.$k.']</a></sup>',$tline);
+            }
+            $outtext[$tkey] = $tline;
+        }
+
+        $out =  join("<br>".PHP_EOL, $outtext);
+        $out .= join("<br>".PHP_EOL, $sources);
+        file_put_contents('sources/test.md', $out);
     }
 ?>
 <form method="post">
