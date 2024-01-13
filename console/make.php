200104aa3212028d91be11ff22e24327949c0c74
Gitanito <nico.spataro@gmail.com>
2024-01-13 20:57:41 +0100

    a

--- console/make.php
+++ console/make.php
@@ -2,12 +2,20 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 
+ini_set('memory_limit', '1024M');
+
+$cleanup = false;
+
+if ($cleanup) {
+    $db->exec("UPDATE settings SET value='0' WHERE name='makingstartpoint';");
+}
 
 $db = new SQLite3('../data/sqlite3database.db');
 $db->exec("DELETE FROM wordcloud;");
 $db->exec("delete from sqlite_sequence where name='wordcloud';");
 
 
+$_wordcloud_index = [];
 
 $all = [];
 $all_ishead = [];
@@ -85,14 +93,15 @@ file_put_contents("../wiki/index.html", $out);
             }
         }
 
+        $startid = ($db->query("SELECT value FROM settings where name='makingstartpoint';"))->fetchArray()[0];
         $entriescount = ($db->query("SELECT count(*) FROM content"))->fetchArray()[0];
-        $entries = $db->query("SELECT * FROM content order by id asc;");
+        $entries = $db->query("SELECT * FROM content order by id asc LIMIT 999999 OFFSET ".$startid.";");
         $ekey = 0;
         while ($entry = $entries->fetchArray(SQLITE3_ASSOC)) {
 
             $intitle = str_replace("\u{00a0}", ' ', trim(explode(',', $entry['mytitle'])[0]));
-            echo $ekey+1 . " von ". $entriescount. " : ".$intitle."\n";
-
+            echo $ekey+$startid+1 . " von ". $entriescount. " : ".$intitle."\n";
+            $db->exec("UPDATE settings SET value='".($startid + $ekey)."' WHERE name='makingstartpoint';");
                 /*
                             // Replaces all spaces with hyphens.
                             $intitle = str_replace(' ', '-', $intitle);
@@ -120,9 +129,11 @@ file_put_contents("../wiki/index.html", $out);
             foreach ($intext as $line) {
                 if (substr($line, 0, 1) === "[") { // this a citation-line
                     $l = explode(" ", $line);
-                    $index = str_replace(['[', ']'], "", $l[0]);
-                    $sources[$index] = '<a name="cit' . $index . '">[' . $index . ']</a> <a target=_blank href="' . trim($l[1]) . '">' . trim($l[1]) . '</a>';
-                    $sourcesl[$index] = '<a target=_blank href="' . trim($l[1]) . '">^</a>';
+                    if (isset($l[1])) {
+                        $index = str_replace(['[', ']'], "", $l[0]);
+                        $sources[$index] = '<a name="cit' . $index . '">[' . $index . ']</a> <a target=_blank href="' . trim($l[1]) . '">' . trim($l[1]) . '</a>';
+                        $sourcesl[$index] = '<a target=_blank href="' . trim($l[1]) . '">^</a>';
+                    }
                 } else {
                     $outtext[] = $line;
                 }
@@ -152,15 +163,12 @@ file_put_contents("../wiki/index.html", $out);
             $matchlist = array_unique(array_values(array_filter($matchlist)));
 
             $match_links = [];
+            $local_wordcloud = [];
             foreach ($matchlist as $ml) {
                 if (trim($ml) != "") {
                     $match_links[$ml] = findPage(strtolower($ml));
-
-                    $stmt  = $db->prepare ("INSERT INTO wordcloud (name,value) values (:name,:value);");
-                    $stmt->bindValue(':name', $filename, SQLITE3_TEXT);
-                    $stmt->bindValue(':value', $ml, SQLITE3_TEXT);
-                    $stmt->execute();
-
+                    $local_wordcloud[] = $ml;
+                    $_wordcloud_index[$ml][] = $filename;
                 }
             }
 
@@ -170,13 +178,14 @@ file_put_contents("../wiki/index.html", $out);
                 array_multisort($keys, SORT_DESC, $match_links);
 
                 $preout = $out;
+                $md5s = [];
                 foreach ($match_links as $kw => $lnk) {
-                    $preout = preg_replace("/" . preg_quote($kw, '/') . "/i", md5($kw), $preout);
+                    $md5s[$kw] = md5($kw);
+                    $preout = preg_replace("/" . preg_quote($kw, '/') . "/i", $md5s[$kw], $preout);
                 }
 
                 foreach ($match_links as $kw => $lnk) {
-                    $z = md5($kw);
-                    $preout = preg_replace("/" . preg_quote($z, '/') . "/i", "<a href='" . $lnk . "'>" . $kw . "</a>", $preout);
+                    $preout = preg_replace("/" . preg_quote($md5s[$kw], '/') . "/i", "<a href='" . $lnk . "'>" . $kw . "</a>", $preout);
                 }
                 $out = $preout;
 
@@ -188,37 +197,23 @@ file_put_contents("../wiki/index.html", $out);
 
             $out_wordcloud = [];
             $out_wordcloud_links = [];
-            $stmt  = $db->prepare ("SELECT value FROM wordcloud WHERE name=:name;");
-            $stmt->bindValue(':name', $filename, SQLITE3_TEXT);
-            $result = $stmt->execute();
-            while ($word = $result->fetchArray()) {
-                $o = findPageName($word[0]);
-                if (isset($o[0]) && !in_array($o[0], $out_wordcloud)) {
-                    $out_wordcloud[] = $o[0];
-                    $out_wordcloud_links[] = "<a href='".$o[1]."'>".$o[0]."</a>&nbsp;<a href='../main_research.php?search=".$o[0]."'><svg xmlns=http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-search' viewBox='0 0 16 16'><path d='M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0'/></svg></a>";
+
+            if (isset($local_wordcloud)) {
+                foreach ($local_wordcloud as $word) {
+                    $o = findPageName($word);
+                    if (isset($o[0]) && !in_array($o[0], $out_wordcloud)) {
+                        $out_wordcloud[] = $o[0];
+                        $out_wordcloud_links[] = "<a href='" . $o[1] . "'>" . $o[0] . "</a>&nbsp;<a href='../main_research.php?search=" . $o[0] . "'><svg xmlns=http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-search' viewBox='0 0 16 16'><path d='M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0'/></svg></a>";
+                    }
                 }
             }
             sort($out_wordcloud_links);
 
             $out =
-                '<!doctype html>
-<html lang="en">
-<head>
-<!-- Required meta tags -->
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="../bootstrap.min.css">
-
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    <script>document.addEventListener("DOMContentLoaded", function(){window.addEventListener("scroll", function() {if (window.scrollY > 50) {document.getElementById("navbar_top").classList.add("fixed-top"); navbar_height = document.querySelector(".navbar").offsetHeight;document.body.style.paddingTop = navbar_height + "px";} else {document.getElementById("navbar_top").classList.remove("fixed-top");document.body.style.paddingTop = "0";}});});</script>
-    <script src="../frontend.js"></script>
-    </head>
-<body><div class="container-fluid">'
+                '<!doctype html><html lang="en"><head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"><link rel="stylesheet" href="../bootstrap.min.css"><script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script><script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script><script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script><script>document.addEventListener("DOMContentLoaded", function(){window.addEventListener("scroll", function() {if (window.scrollY > 50) {document.getElementById("navbar_top").classList.add("fixed-top"); navbar_height = document.querySelector(".navbar").offsetHeight;document.body.style.paddingTop = navbar_height + "px";} else {document.getElementById("navbar_top").classList.remove("fixed-top");document.body.style.paddingTop = "0";}});});</script><script src="../frontend.js"></script></head><body><div class="container-fluid">'
                 . '<div class="card" style="width:100%;"><div class="card-header" id="navbar_top"><h1>'.trim($intitle).'</h1>'
                 . (isset($entry['mylink'])?"Link: <a target=_blank href='".$entry['mylink']."'>".$entry['mylink']."</a><br><br>":"")
+                . '<p id="filefunctions" data-filename="'.$entry['id'].'.txt"></p>'
                 . '</div><div class="card-body">'
                 . '<div class="card" style="width:20%;float:right; margin-left:20px;"><div class="card-header"><h3>Relevante Themen</h3></div><div class="card-body"><ul><li>'
                 . join("</li><li>", $out_wordcloud_links)
@@ -231,5 +226,16 @@ file_put_contents("../wiki/index.html", $out);
             file_put_contents('../wiki/'. $filename . ".html", $out);
             $ekey++;
         }
+
+        echo "\n\nWriting WordCloud.\n";
+        $s = 0;
+        foreach ($_wordcloud_index as $keyword => $subs ) {
+            $stmt = $db->prepare("INSERT INTO wordcloud (name,value) values (:name,:value);");
+            $stmt->bindValue(':name', $keyword, SQLITE3_TEXT);
+            $stmt->bindValue(':value', json_encode($subs), SQLITE3_TEXT);
+            $stmt->execute();
+            echo $s++ . " of " . sizeof($_wordcloud_index) . " written.\n";
+        }
+
 
     } catch (Exception $e) {}
